fb1c839bd69ae6e14fca08fe8e7a87247ceb7942
RohaanUmair <m.rohaanumair@gmail.com>
2024-05-04 01:25:17 +0500

    Stop Watch

--- main.js
+++ main.js
@@ -2,20 +2,35 @@ const hours = document.getElementById('hour');
 const mins = document.getElementById('min');
 const sec = document.getElementById('sec');
 const ms = document.getElementById('ms');
+const startBtn = document.getElementById('start-btn');
 
 
-let date = Date.now();
+let miliSec;
 let start;
 function startTimer(){
-    start = setInterval(() => {
-        let timeElapsed = Date.now() - date;
-        ms.innerText = timeElapsed;
+    startBtn.disabled = true;
 
-        if (ms.innerText >= 999){
-            ms.innerText = 0;
+
+    let mSec = 0;
+
+    miliSec = setInterval(() => {
+        mSec += 10;
             
-            sec.innerText = (sec.innerText < 9) ? '0' + ++sec.innerText : ++sec.innerText;
+        let formattedMs = (ms.innerText < 9) ? '0' + mSec : mSec;
+
+
+        ms.innerText = formattedMs;
+    
+        if (mSec >= 990) {
+            mSec = 0;
         }
+
+    }, 10);
+
+
+
+    start = setInterval(() => {
+        sec.innerText = (sec.innerText < 9) ? '0' + ++sec.innerText : ++sec.innerText;
 
         if (sec.innerText == 59){
             sec.innerText = 0;
@@ -32,24 +47,24 @@ function startTimer(){
         if (hours.innerText == 99){
             clearInterval(start);
         }
-    }, 10);
+    }, 1000);
 
 }
 
 
 function stopTimer(){
     clearInterval(start);
+    clearInterval(miliSec);
+    startBtn.disabled = false;
 }
 
 
 function resetTimer(){
     clearInterval(start);
-    ms.innerText = '000';
+    clearInterval(miliSec);
+    startBtn.disabled = false;
     sec.innerText = '00';
     mins.innerText = '00';
     hours.innerText = '00';
+    ms.innerText = '000'
 }
-
-
-
-
